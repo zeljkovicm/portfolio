be596024101121313b5a5bf8f9bffd222ccd4e40
zeljkovicm <zeljkovic_milan@yahoo.com>
2026-04-13 02:36:40 +0200

    Adding all icons

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,21 +1,41 @@
+import python from '../assets/icons/python-original.svg';
+import fastapi from '../assets/icons/fastapi-original.svg';
+import typescript from '../assets/icons/typescript-original.svg';
+import postgresql from '../assets/icons/postgresql-original.svg';
+import mongodb from '../assets/icons/mongodb-original.svg';
+import react from '../assets/icons/react-original.svg';
+import html5 from '../assets/icons/html5-original.svg';
+import css3 from '../assets/icons/css3-original.svg';
+import angular from '../assets/icons/angular-original.svg';
+import nodejs from '../assets/icons/nodejs-original.svg';
+import java from '../assets/icons/java-original.svg';
+import tensorflow from '../assets/icons/tensorflow-original.svg';
+import jupyter from '../assets/icons/jupyter-original.svg';
+import azure from '../assets/icons/azure-original.svg';
+import fluentbit from '../assets/icons/fluentbit-original.svg';
+import lua from '../assets/icons/lua-original.svg';
+import postman from '../assets/icons/postman-original.svg';
+import powershell from '../assets/icons/powershell-original.svg';
+import prometheus from '../assets/icons/prometheus-original.svg';
+
 export const skills = [
-    { name: 'Python', icon: '/src/assets/icons/python-original.svg', category: 'Backend' },
-    { name: 'FastAPI', icon: '/src/assets/icons/fastapi-original.svg', category: 'Backend' },
-    { name: 'TypeScript', icon: '/src/assets/icons/typescript-original.svg', category: 'Backend' },
-    { name: 'PostgreSQL', icon: '/src/assets/icons/postgresql-original.svg', category: 'Backend' },
-    { name: 'MongoDB', icon: '/src/assets/icons/mongodb-original.svg', category: 'Backend' },
-    { name: 'React', icon: '/src/assets/icons/react-original.svg', category: 'Frontend' },
-    { name: 'HTML5', icon: '/src/assets/icons/html5-original.svg', category: 'Frontend' },
-    { name: 'CSS3', icon: '/src/assets/icons/css3-original.svg', category: 'Frontend' },
-    { name: 'Angular', icon: '/src/assets/icons/angular-original.svg', category: 'Frontend' },
-    { name: 'Node.js', icon: '/src/assets/icons/nodejs-original.svg', category: 'Backend' },
-    { name: 'Java', icon: '/src/assets/icons/java-original.svg', category: 'Backend' },
-    { name: 'TensorFlow', icon: '/src/assets/icons/tensorflow-original.svg', category: 'Other' },
-    { name: 'Jupyter', icon: '/src/assets/icons/jupyter-original.svg', category: 'Other' },
-    { name: 'Azure', icon: '/src/assets/icons/azure-original.svg', category: 'DevOps' },
-    { name: 'Fluent Bit', icon: '/src/assets/icons/fluentbit-original.svg', category: 'DevOps' },
-    { name: 'Lua', icon: '/src/assets/icons/lua-original.svg', category: 'Other' },
-    { name: 'Postman', icon: '/src/assets/icons/postman-original.svg', category: 'Other' },
-    { name: 'PowerShell', icon: '/src/assets/icons/powershell-original.svg', category: 'DevOps' },
-    { name: 'Prometheus', icon: '/src/assets/icons/prometheus-original.svg', category: 'DevOps' },
+    { name: 'Python', icon: python, category: 'Backend' },
+    { name: 'FastAPI', icon: fastapi, category: 'Backend' },
+    { name: 'TypeScript', icon: typescript, category: 'Backend' },
+    { name: 'PostgreSQL', icon: postgresql, category: 'Backend' },
+    { name: 'MongoDB', icon: mongodb, category: 'Backend' },
+    { name: 'React', icon: react, category: 'Frontend' },
+    { name: 'HTML5', icon: html5, category: 'Frontend' },
+    { name: 'CSS3', icon: css3, category: 'Frontend' },
+    { name: 'Angular', icon: angular, category: 'Frontend' },
+    { name: 'Node.js', icon: nodejs, category: 'Backend' },
+    { name: 'Java', icon: java, category: 'Backend' },
+    { name: 'TensorFlow', icon: tensorflow, category: 'Other' },
+    { name: 'Jupyter', icon: jupyter, category: 'Other' },
+    { name: 'Azure', icon: azure, category: 'DevOps' },
+    { name: 'Fluent Bit', icon: fluentbit, category: 'DevOps' },
+    { name: 'Lua', icon: lua, category: 'Other' },
+    { name: 'Postman', icon: postman, category: 'Other' },
+    { name: 'PowerShell', icon: powershell, category: 'DevOps' },
+    { name: 'Prometheus', icon: prometheus, category: 'DevOps' },
 ]
